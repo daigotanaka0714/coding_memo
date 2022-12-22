83ceb31f7e59cfa69968038360d363b625020dc2
daigotanaka0714 <daigo.tanaka0714@gmail.com>
2022-12-22 10:36:19 +0900

    remove loadingGears from app.vue

--- app.vue
+++ app.vue
@@ -1,22 +1,5 @@
-<script setup>
-import LoadingGears from '~/components/LoadingGears.vue'
-const nuxtApp = useNuxtApp()
-const loading = ref(false)
-nuxtApp.hook('page:start', () => {
-  loading.value = true
-})
-nuxtApp.hook('page:finish', () => {
-  loading.value = false
-})
-</script>
-
 <template>
-  <div>
-    <div v-if="loading">
-      <LoadingGears />
-    </div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
