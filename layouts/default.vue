5b22aa88abe1e9b8da34b931d41684fc6f0a0330
daigotanaka0714 <daigo.tanaka0714@gmail.com>
2022-12-22 10:12:56 +0900

    add header and footer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Header from '~~/components/topHeader.vue'
+import Footer from '~~/components/bottomFooter.vue'
+</script>
 
 <template>
   <div>
+    <Header />
     <slot />
+    <Footer />
   </div>
 </template>
 
