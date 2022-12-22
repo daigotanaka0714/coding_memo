2d835e379e57ac303befcd58bc4e53e00b691ccf
daigotanaka0714 <daigo.tanaka0714@gmail.com>
2022-12-22 10:28:24 +0900

    add loading

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
+<script setup>
+import LoadingGears from '~/components/LoadingGears.vue'
+const nuxtApp = useNuxtApp()
+const loading = ref(false)
+nuxtApp.hook('page:start', () => {
+  loading.value = true
+})
+nuxtApp.hook('page:finish', () => {
+  loading.value = false
+})
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div>
+    <div v-if="loading">
+      <LoadingGears />
+    </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
